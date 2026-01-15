30af75424d353a32f40ceb88dd7d936c6d399d7e
Prasanna R <prasanna.r@opsera.io>
2026-01-15 19:56:35 +0530

    Update Hello.java

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Mono;
 import java.util.function.Function;
 
 @Component
-public class Hello implements Function<Mono<User>, Mono<Greeting>> {
+public class Hello implements Function<User, Greeting> {
 
     public Mono<Greeting> apply(Mono<User> mono) {
         return mono.map(user -> new Greeting("Hello, " + user.getName() + "!\n"));
